b32b5eaf46b93e189071d573eda322cf3fe17616
Karl Jason Manait <karlbmanait@gmail.com>
2022-09-16 23:14:22 +0800

    kube: Update setup script for v1.25

--- kubernetes/setup-kubernetes.sh
+++ kubernetes/setup-kubernetes.sh
@@ -9,9 +9,25 @@ done
 # Turn off swap
 sudo swapoff -a
 
-# Do not disable CRI
-sudo rm /etc/containerd/config.toml
+# Kernel settings
+sudo tee /etc/modules-load.d/containerd.conf <<EOF
+overlay
+br_netfilter
+EOF
+sudo modprobe overlay
+sudo modprobe br_netfilter
+sudo tee /etc/sysctl.d/kubernetes.conf <<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+sudo sysctl --system
+
+# Containerd setup
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
 sudo systemctl restart containerd
+sudo systemctl enable containerd
 
 # Initialize cluster master
 read -p "Enter cluster CIDR (default: 192.168.0.0/16): " cidr
@@ -27,13 +43,18 @@ fi
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl cluster-info
+sleep 5s
+kubectl get nodes
+sudo journalctl -u containerd -e | tail -n 3
+sudo journalctl -u kubelet -e | tail -n 3
 
 # Install calico
-kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.24.1/manifests/tigera-operator.yaml
 if [[ ${cidr} == "192.168.0.0/16" ]]; then
-  kubectl create -f https://docs.projectcalico.org/manifests/custom-resources.yaml
+  kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.24.1/manifests/custom-resources.yaml
 else
-  wget https://docs.projectcalico.org/manifests/custom-resources.yaml
+  wget https://raw.githubusercontent.com/projectcalico/calico/v3.24.1/manifests/custom-resources.yaml
   sed -i "s|192.168.0.0/16|${cidr}|g" ./custom-resources.yaml
   kubectl apply -f ./custom-resources.yaml
   rm ./custom-resources.yaml
