38108869415e81d33d0a48b777ba66bba7a5ece6
Karl Jason Manait <karlbmanait@gmail.com>
2024-04-02 11:43:46 +0800

    kube: Support installation on debian

--- kubernetes/install-kubernetes.sh
+++ kubernetes/install-kubernetes.sh
@@ -5,9 +5,24 @@ sudo apt install -y apt-transport-https gnupg2 curl
 # Add GPG key
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
+# Determine distro
+if [ -f /etc/os-release ]; then
+  . /etc/os-release
+  distro=$NAME
+  echo "Detected Linux distro: ${distro}"
+else
+  echo "Unable to determine Linux distro"
+  read -p "Enter your distro (Debian/Ubuntu): " distro
+fi
 # Add repository
-kube_repo="deb https://apt.kubernetes.io/ kubernetes-xenial main"
-echo $kube_repo | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+if [[ $distro =~ "Debian" ]]; then
+  kube_repo="deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /"
+  echo $kube_repo | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+  curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+elif [[ $distro =~ "Ubuntu" ]]; then
+  kube_repo="deb https://apt.kubernetes.io/ kubernetes-xenial main"
+  echo $kube_repo | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+fi
 
 # Install kubernetes components
 sudo apt update
